592745a1c88c17b6cd7a5ce306fb02b1a83acba0
rafaelwinter1201 <rafael.winter.felipe@gmail.com>
2023-10-10 22:53:54 -0300

    aula 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log ('Oi console')
+    }, 5000);
+  }
 }
